66f7d3bd3e83bd3205aa251d592200be636525d7
Robério <rroberiosouza@gmail.com>
2022-01-10 22:04:50 -0300

    definido pasta uploads como publica dentro do projeto.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const routeProducts = require('./routes/products');
 const routeOrders = require('./routes/orders');
 
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json());
 
